103eb0c5982869b064e2568dae2766db8c5e81b6
gcw_26OXmmvv <gcw_26OXmmvv@noreply.gitcode.com>
2026-08-01 11:30:57 +0800

    matlab自动校准

--- Tools/MATLAB/UTG2062X_AutoCalibration/h743_parser_init.m
+++
@@ -1,9 +0,0 @@
-function state = h743_parser_init()
-%H743_PARSER_INIT Initialize persistent stream parser state.
-
-state.Buffer = uint8([]);
-state.CrcErrors = uint32(0);
-state.FormatErrors = uint32(0);
-state.FramesDecoded = uint32(0);
-end
-
